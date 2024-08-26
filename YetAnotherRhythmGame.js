19577529910e2889dbc1d26e5912abaf1c450d73
Sreekar Ramisetty <115044820+Sreekar617@users.noreply.github.com>
2024-08-25 19:04:51 -0500

    added player and movement

--- YetAnotherRhythmGame.js
+++ YetAnotherRhythmGame.js
@@ -1,1 +1,128 @@
 
+/*
+First time? Check out the tutorial game:
+https://sprig.hackclub.com/gallery/getting_started
+
+@title: osu
+@author: 
+@tags: []
+@addedOn: 2024-00-00
+*/
+
+const player = "p"
+const wall = "w"
+const bg = "b"
+const apple = "a"
+
+setLegend(
+  [player, bitmap`
+......3333......
+....33333333....
+...333333333....
+...333333333....
+.77777733333....
+.7227773333333..
+.7777773333333..
+...33300000333..
+...33303333333..
+...33330333333..
+...333330333....
+...333303333....
+...333033333....
+...333000003....
+....33....33....
+....33....33....`],
+  [wall, bitmap`
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH`],
+  [bg, bitmap`
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000`],
+  [apple, bitmap`
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333`]
+)
+
+setSolids([])
+
+let level = 0
+const levels = [
+  map`
+w....w
+w....w
+w....w
+w....w
+w....w
+w....w
+w....w
+wp...w`
+]
+setBackground(bg)
+
+setMap(levels[level])
+
+setPushables({
+  [player]: []
+})
+
+onInput("s", () => {
+  getFirst(player).x = 1
+})
+
+onInput("d", () => {
+  getFirst(player).x = 2
+})
+
+onInput("k", () => {
+  getFirst(player).x = 3
+})
+
+onInput("l", () => {
+  getFirst(player).x = 4
+})
+
+afterInput(() => {
+  console.log(tilesWith(player, apple))
+})
